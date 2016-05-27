f0e2e9775f22544caf1e3f24b161841a22fbcc79
Davide Gallitelli <davidegallitelli@gmail.com>
2016-05-27 15:24:29 +0200

    Other updates to chat installer

--- trabajo_final/chatinstall.sh
+++ trabajo_final/chatinstall.sh
@@ -3,4 +3,6 @@
 cd ~ && mkdir chat && cd chat
 wget https://github.com/UGR2015IT/SWAP/raw/master/trabajo_final/Other%20Files/chat.rar
 unrar e -x chat.rar
-cd chat
+rm chat.rar
+npm install
+npm install express socket.io
